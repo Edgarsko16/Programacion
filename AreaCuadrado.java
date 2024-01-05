a901aff88ff6d165ffc4838d9e678ffc05627580
Edgar José Naranjo González <151402129+Edgarsko16@users.noreply.github.com>
2024-01-05 20:31:28 +0000

    AreaCuadrado.java

--- AreaCuadrado.java
+++ AreaCuadrado.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        /* Escribe un programa en Java que lea un número y calcule el área de un cuadrado (Área del cuadrado = lado * lado) */
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Dame un número");
+        int numero = lectura.nextInt();
+        int areaCuadrado = numero * numero;
+        System.out.println("El área de tu cuadrado es " + areaCuadrado);
+    }
+}
